f23af1b9a44e856e55c15149daf66d4bb9c99d1e
abdurahman tanatar <abdurahman.t.2000@gmail.com>
2024-04-13 18:25:20 +0500

    add product model

---
+++ product-service/src/models/product_model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+    {
+        product_name: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        is_deleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Products', ProductSchema);
