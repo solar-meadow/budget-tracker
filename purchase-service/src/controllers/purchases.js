344c5e44d4b6941c8ae75430145904febc9503a1
abdurahman tanatar <abdurahman.t.2000@gmail.com>
2024-04-17 07:42:29 +0500

    add mongo crud

--- purchase-service/src/controllers/purchases.js
+++ purchase-service/src/controllers/purchases.js
@@ -1,17 +1,89 @@
-const getAllPurchases = (req, res) => {
-    res.send('this is get all purchases');
+import e from 'express';
+import PurchaseModel from '../models/purchase.js';
+
+const getAllPurchases = async (req, res) => {
+    const purchase = await PurchaseModel.find({ isDeleted: false });
+    if (!purchase) {
+        res.sendStatus(404);
+    }
+    res.send(purchase);
 };
-const getPurchaseById = (req, res) => {
-    res.send('this is get purchase by id');
+
+const getPurchaseById = async (req, res) => {
+    const purchase = await PurchaseModel.findById(req.params.id);
+    if (!purchase) {
+        res.sendStatus(404);
+    }
+    res.send(purchase);
 };
-const createPurchase = (req, res) => {
-    res.send('this is create purchase');
+
+const createPurchase = async (req, res) => {
+    const products = req.body.products.map((product) => ({
+        product_id: product.product_id,
+        price: product.price,
+        quantity: product.quantity,
+    }));
+
+    const doc = new PurchaseModel({
+        products: products,
+        user_id: req.body.user_id,
+    });
+    try {
+        const purchase = await doc.save();
+        res.status(200);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+        return;
+    }
+    res.sendStatus(200);
 };
-const updatePurchaseById = (req, res) => {
-    res.send('this is update  purchase');
+const updatePurchaseById = async (req, res) => {
+    const purchase = await PurchaseModel.findById(req.params.id);
+    if (!purchase) {
+        res.sendStatus(404);
+    }
+    try {
+        const products = req.body.products.map((product) => ({
+            product_id: product.product_id,
+            price: product.price,
+            quantity: product.quantity,
+        }));
+        const updatedPurchase = await PurchaseModel.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+            }
+        );
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+        return;
+    }
+    res.sendStatus(200);
 };
-const deletePurchaseById = (req, res) => {
-    res.send('this is delete purchase');
+const deletePurchaseById = async (req, res) => {
+    const purchase = await PurchaseModel.findById(req.params.id);
+    if (!purchase) {
+        res.sendStatus(404);
+    }
+    try {
+        const updatedPurchase = await PurchaseModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                isDeleted: true,
+            },
+            {
+                new: true,
+            }
+        );
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+        return;
+    }
+    res.sendStatus(200);
 };
 
 export default {
