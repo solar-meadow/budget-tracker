91d167b3372158132b170b30014dce459516db24
Bakdaulet Ye <b.yelmurzayev@gmail.com>
2024-05-10 23:52:17 +0500

    WIP purchase

--- gateway/src/controllers/purchase.controller.js
+++ gateway/src/controllers/purchase.controller.js
@@ -2,12 +2,15 @@ import { PurchaseService } from "../services/purchase.service.js";
 import { HttpClient } from "../services/http-client.service.js";
 
 const getMyDebts = async (req, res) => {
+  const { userId } = req.query;
+  if (!userId) {
+    res.status(400);
+    return;
+  }
+  const url = new URL(process.env.PURCHASES_SERVICE);
+  url.searchParams.set(req.query.userId);
   const httpClient = new HttpClient();
-
-  const purchaseService = new PurchaseService(
-    httpClient,
-    `${process.env.PURCHASES_SERVICE}/create`
-  );
+  const purchaseService = new PurchaseService(httpClient, url);
 
   try {
     const result = await purchaseService.get(payload);
